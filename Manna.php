fb4db938c89eb1f9c07f08895c469b37e3d3a927
Michael Ten <scott71@users.noreply.github.com>
2018-09-01 16:02:08 -0700

    Create Manna.php

---
+++ Manna.php
@@ -0,0 +1,6 @@
+/* This will be a MediaWiki extension to accept membership dues in the form of the cryptocurrency Manna.  */
+/* You can sign up to receive Manna at MannaBase.com */
+/* A GitCoin bounty will be created on Gitcoin.co */
+/* Membership dues can be paid per month, or for the year. */
+/* View specifications: https://github.com/Tenoorja/Manna/wiki/Manna-and-MediaWiki */
+/* Once you have successfully demonstrated a functional MediaWiki extension that sufficiently matches the described specifications, the bounty will be paid. */
